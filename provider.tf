fab979c8f3649cb44d4e7f60087197581984c254
venkatpolali <105282566+venkatpolali@users.noreply.github.com>
2024-09-16 11:08:44 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.66.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "6.2.0"
+    }
+    oci = {
+      source = "oracle/oci"
+      version = "6.9.0"
+    }
+     kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+  }
+}
